ca67b7f082a65a50a13dd99fd5e5837e07e7e7a4
GBTC405011UR <GBTC405011UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-19 14:32:57 -0400

    Added two variables from exercises page 13

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,9 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
+   // private Object s;
+
     @RequestMapping("/")
     public String homepage(Model model){
         model.addAttribute("myvar","Say hello to the people.");
+        model.addAttribute("myvar2", "greeting");
+        //model.addAttribute( s: "myvar3", o: "flipslopos");
         return "hometemplate";
     }
 }
